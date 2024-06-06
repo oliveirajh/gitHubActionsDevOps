24619d0306a4a7fe3cadb57dda9c7265ce21dda1
Rodrigo Braz Rodrigues <rodrigo.braz@venturus.org.br>
2024-06-06 10:39:13 -0300

    Teste unitario com Pass

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 console.log("Hello, GitHub Actions");
 
 function division(a, b) {
-    // if (b === 0) {
-    //     throw new Error("Division by zero is not allowed");
-    // }
+    if (b === 0) {
+        throw new Error("Divisão por zero não é permitida");
+    }
     return a / b;
 }
 
