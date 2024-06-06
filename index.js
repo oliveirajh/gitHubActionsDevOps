72c7fc0544b5d3df25993a34d8e09856936b2d73
Rodrigo Braz Rodrigues <rodrigo.braz@venturus.org.br>
2024-06-06 17:53:15 -0300

    teste com falha proposital

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 console.log("Hello, GitHub Actions");
 
 function division(a, b) {
-    if (b === 0) {
-        throw new Error("Divisão por zero não é permitida");
-    }
+    // if (b === 0) {
+    //     throw new Error("Divisão por zero não é permitida");
+    // }
     return a / b;
 }
 
